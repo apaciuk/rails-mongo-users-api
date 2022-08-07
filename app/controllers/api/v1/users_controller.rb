4f1b9a616964e9090ff7f32c0145e7a9eee84147
Paul Anthony McGowan <xhostcom@gmail.com>
2022-08-07 11:49:05 +0100

    Refactor controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,15 @@ module Api
         render json: @users, status => 200
       end
 
+      def show
+        @user = User.find(params[:id])
+        if @user
+          render json: @user, status => 200
+        else
+          render json: { error: 'User not found' }, status => 404
+        end
+      end
+
       def create
         user = User.new(user_params)
         if user.save
