1f8588240d9c58066d2659b6cf50b75603bca13e
Paul Anthony McGowan <xhostcom@gmail.com>
2022-08-07 15:23:06 +0100

    Encrypt Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,16 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  include ActiveModel::SecurePassword
+  include BCrypt
 
   field :username, type: String, default: ''
   field :email, type: String, default: ''
   field :password_digest, type: String, default: ''
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true, length: { minimum: 9 }, on: :create
+
+  has_secure_password
 end
